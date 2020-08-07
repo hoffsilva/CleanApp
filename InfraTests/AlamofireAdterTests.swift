63a83707f79bc113dfa77d4f3d3731565cb3774a
Hoff Silva <hoff.henry@gmail.com>
2020-08-06 21:23:46 -0300

    Create infra layer

---
+++ InfraTests/AlamofireAdterTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireAdterTests.swift
+//  AlamofireAdterTests
+//
+//  Created by Hoff Henry Pereira da Silva on 06/08/20.
+//  Copyright © 2020 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdterTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
