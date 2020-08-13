f2e6755cee718c30c3c85822afd2a100ad400245
Hoff Silva <hoff.henry@gmail.com>
2020-08-13 15:18:13 -0300

    WIP - Add tests for alamofire adapter

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -18,15 +18,18 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(to url: URL, with data: Data) {
-        let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
-        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+    func post(to url: URL, with data: Data?) {
+        if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+            session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+        } else {
+            session.request(url, method: .post, parameters: nil, encoding: JSONEncoding.default).resume()
+        }
     }
 }
 
 class AlamofireAdapterTests: XCTestCase {
 
-    func test_post_shoul_make_request_with_valid_url_and_methd() {
+    func test_post_should_make_request_with_valid_url_and_methd() {
         let expect = expectation(description: "waiting")
         let url = TestTools.createUrl()
         let sessionConfiguration = URLSessionConfiguration.default
@@ -44,6 +47,21 @@ class AlamofireAdapterTests: XCTestCase {
         }
         wait(for: [expect], timeout: 0.1 )
     }
+    
+     func test_post_should_make_request_with_no_data() {
+            let expect = expectation(description: "waiting")
+            let url = TestTools.createUrl()
+            let sessionConfiguration = URLSessionConfiguration.default
+            sessionConfiguration.protocolClasses = [URLProtocolStub.self]
+            let session = Session(configuration: sessionConfiguration)
+            let sut = AlamofireAdapter(session: session)
+            sut.post(to: url, with: nil)
+            URLProtocolStub.requestObserver { request in
+                XCTAssertNil(request.httpBodyStream)
+                expect.fulfill()
+            }
+            wait(for: [expect], timeout: 1 )
+        }
 
 }
 
