e6a3f5c86da50a5f01d9c79f28e67cb8470e9e18
Hoff Silva <hoff.henry@gmail.com>
2020-08-25 08:21:22 -0300

    Add Presentation skeleton

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpPresenterTests.swift
+//  SignUpPresenterTests
+//
+//  Created by Hoff Henry Pereira da Silva on 25/08/20.
+//  Copyright © 2020 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import XCTest
+@testable import Presentation
+
+class SignUpPresenterTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
