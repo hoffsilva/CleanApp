887651c3cb1395c8fd24864f20e0ddc49c6740ca
Hoff Silva <hoff.henry@gmail.com>
2020-09-18 18:25:11 -0300

    Add test for invalids emails

---
+++ ValidationTests/ValidationTests.swift
@@ -0,0 +1,19 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  EmailValidatorAdapterTests
+//
+//  Created by Hoff Henry Pereira da Silva on 18/09/20.
+//  Copyright © 2020 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import XCTest
+@testable import Validation
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
