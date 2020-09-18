aae0f77e5b161df091c0bf85c0ec729be76cab98
Hoff Silva <hoff.henry@gmail.com>
2020-09-18 18:06:26 -0300

    Add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  EmailValidatorAdapterTests
+//
+//  Created by Hoff Henry Pereira da Silva on 18/09/20.
+//  Copyright © 2020 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func testExample() {
+        
+    }
+
+}
