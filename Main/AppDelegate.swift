831896b3bcd6f408ae8d6f668be93b9566eecf64
Hoff Silva <hoff.henry@gmail.com>
2020-09-19 11:18:27 -0300

    Add main target

--- Main/AppDelegate.swift
+++ Main/AppDelegate.swift
@@ -1,28 +1,29 @@
 //
 //  AppDelegate.swift
-//  UI
+//  Main
 //
-//  Created by Hoff Henry Pereira da Silva on 15/09/20.
+//  Created by Hoff Henry Pereira da Silva on 19/09/20.
 //  Copyright © 2020 Hoff Henry Pereira da Silva. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let signUpViewController = SignUpViewController(signUpView: SignUpView())
-        window.rootViewController = signUpViewController
-        
-        self.window = window
+//        let signUpViewController = SignUpViewController(signUpView: SignUpView())
+//        window.rootViewController = signUpViewController
+//        
+//        self.window = window
         window.makeKeyAndVisible()
         return true
     }
-    
+
+
 }
+
